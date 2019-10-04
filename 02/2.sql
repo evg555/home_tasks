a4ed6a7ed4e4ecdf7f24c81a2d04ccbe23aed1be
Evgeniy Krylov <ontropos@gmail.com>
2019-10-04 23:03:07 +0300

    Added task 02

---
+++ 02/2.sql
@@ -0,0 +1,12 @@
+/*
+* 2. Создайте базу данных example, разместите в ней таблицу users, состоящую из двух столбцов, 
+* числового id и строкового name.
+*/
+
+CREATE DATABASE IF NOT EXISTS example;
+USE example;
+
+CREATE TABLE users (
+	id INT,
+	name VARCHAR(255)
+);
