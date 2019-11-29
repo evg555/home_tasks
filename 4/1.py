4ca8d2da7b17f8ca1ef4b935a0cd965021ea3154
Evgeniy Krylov <ontropos@gmail.com>
2019-11-29 15:21:34 +0300

    Added hometask 04

--- 4/1.py
+++ 4/1.py
@@ -1,16 +1,17 @@
+# Решите аналитически и потом численно (в программе) уравнение, зависящее от параметра а:
+# sin(а*x)=0
+# при условии: 0.01<a<0.02, 100<х<500.
+# Т.е. надо найти решение х как функцию параметра а - построить график x=x(а).
+# Если численным методом не получается найти все ветви решения x(а), то отыщите хотя бы одну.
 import numpy as np
 import matplotlib.pyplot as plt
-import math
-x = []
-y = []
 
-for i in range(-3.14,math.pi, 3.14):
-    x_ = i/100000
-    x.append(x_)
-    y1.append(math.sqrt((1+x_**2)/a**2)*b**2)
-    y2.append(-math.sqrt((1 + x_ ** 2) / a ** 2) * b ** 2)
-y = math.sin(x)/x
+x = np.linspace(100, 500, 100)
+n = range(0,100,1)
+y = [np.sin(0.01*i) for i in x]
+y2 = [np.sin(0.02*i) for i in x]
+
 plt.plot(x,y)
-plt.xlabel('x')
-plt.ylabel('y')
+plt.plot(x,y2)
 plt.grid()
+plt.show()
