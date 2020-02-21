fef5cacc2bb85326430b7219ff08beabee3612ea
Evgeniy Krylov <ontropos@gmail.com>
2020-02-21 16:45:56 +0700

    Add lesson 8

--- lesson7/mvideo.py
+++ lesson7/mvideo.py
@@ -15,17 +15,20 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://mvideo.ru/')
 assert 'М.Видео' in driver.title
 
-WebDriverWait(driver, 20).until(
-	EC.frame_to_be_available_and_switch_to_it("fl-306642")
-)
-driver.find_element_by_class_name("close").click()
-driver.switch_to.default_content()
+try:
+	WebDriverWait(driver, 20).until(
+		EC.frame_to_be_available_and_switch_to_it("fl-306642")
+	)
+	driver.find_element_by_class_name("close").click()
+	driver.switch_to.default_content()
+except:
+	print('Всплывающеее окно не найдено')
 
 
 hit_items = WebDriverWait(driver, 10).until(
-	EC.presence_of_element_located((By.CLASS_NAME, "sel-hits-block"))
+	EC.presence_of_all_elements_located((By.CLASS_NAME, "accessories-carousel-holder"))
 )
 
 actions = ActionChains(driver)
-actions.move_to_element(hit_items).perform()
+actions.move_to_element(hit_items[2]).perform()
 arrow = driver.find_element_by_class_name("sel-hits-button-next").click()
