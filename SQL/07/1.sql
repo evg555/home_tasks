59d73dbf392770a526927481dd173450f0d82cff
Evgeniy Krylov <ontropos@gmail.com>
2020-03-01 11:48:15 +0700

    first commit

--- SQL/07/1.sql
+++
@@ -1,5 +0,0 @@
-/*
- * Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
- */ 
-
-select u.id, u.name from users u join orders o on u.id = o.user_id group by u.id
